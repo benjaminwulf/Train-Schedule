523875cca2bfde5ea5ca53c8d45b2538c347f878
Benjamin W Wulf <benjaminwulf@gmail.com>
2018-07-02 01:50:39 -0600

    got form data to display to card

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-3EiHgygh66XAfl37NZe_neMgBQCcOSc",
+    authDomain: "train-schedule-d5b60.firebaseapp.com",
+    databaseURL: "https://train-schedule-d5b60.firebaseio.com",
+    projectId: "train-schedule-d5b60",
+    storageBucket: "",
+    messagingSenderId: "213660198524"
+  };
+  firebase.initializeApp(config);
+
+// Reference form 
+var formRef = firebase.database().ref('train-form');
+
+// Listen for form submit
+// In vanilla js
+document.getElementById('train-form').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    console.log(123);
+
+    // Get values
+    var name = getInputVal('name');
+    var destination = getInputVal('destination');
+    var firstTrain = getInputVal('firstTrain');
+    var frequency = getInputVal('frequency');
+
+    // Save Form
+    saveForm(name, destination, firstTrain, frequency);
+};
+
+// In jQuery //bww_not_working
+//
+// code goes here
+//
+
+// Function to get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save form to firebase
+function saveForm(name, destination, firstTrain, frequency) {
+    var newFormRef = formRef.push();
+    newFormRef.set({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+}
+
+// Display to Current Train Schedule in Table
+var rootRef = firebase.database().ref().child("train-form")
+
+rootRef.on("child_added", snap => {
+    var displayName = snap.child("name").val();
+    var displayDestination = snap.child("destination").val();
+    var displayFirstTrain = snap.child("firstTrain").val();
+    var displayFequency = snap.child("frequency").val();
+
+    $("#train-schedule").append("<tr><td>" + displayName + "</td><td>" + displayDestination + "</td><td>" + displayFirstTrain + "</td><td>" + displayFequency + "</td>")
+});
